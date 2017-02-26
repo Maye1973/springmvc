415064ab5cdda73b2b68eb15dbd4a478e17d923d
Maye1973 <2012099820@qq.com>
2017-02-26 09:31:43 +0800

    Rejected commit: Default

--- src/main/java/com/mamlong/springmvc/demo/controller/DemoController.java
+++ src/main/java/com/mamlong/springmvc/demo/controller/DemoController.java
@@ -1,11 +1,10 @@
 package com.mamlong.springmvc.demo.controller;
 
 import com.mamlong.springmvc.demo.service.IDemoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.condition.ProducesRequestCondition;
-
-import java.util.Date;
 
 /**
  * Created by MaMingLong on 2017/1/14.
@@ -15,12 +14,15 @@ import java.util.Date;
 @RequestMapping("/demo")
 public class DemoController {
 
+    private static final Logger logger = LoggerFactory.getLogger(DemoController.class);
+
     @Autowired
     private IDemoService demoService;
 
     @RequestMapping(value = "/sayHello/{name}", produces = {"application/json;charset=UTF-8"})
     @ResponseBody
     public String sayHello(@PathVariable(value = "name") String name){
+        logger.info("DemoController.sayHello入参：" + name);
         return "hello " + name + " now is " + demoService.getCurrentDate();
     }
 
