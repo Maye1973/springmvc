b21a6812ea20757885aff64faef7f2712f22b78b
Maye1973 <2012099820@qq.com>
2017-05-13 11:04:32 +0800

    test spring circular reference is ok

--- src/main/java/com/mamlong/springmvc/demo/service/impl/DemoServiceImpl.java
+++ src/main/java/com/mamlong/springmvc/demo/service/impl/DemoServiceImpl.java
@@ -1,6 +1,8 @@
 package com.mamlong.springmvc.demo.service.impl;
 
 import com.mamlong.springmvc.demo.service.IDemoService;
+import com.mamlong.springmvc.swagger.service.ISwaggerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -10,8 +12,13 @@ import java.util.Date;
  */
 @Service
 public class DemoServiceImpl implements IDemoService{
+
+    @Autowired
+    private ISwaggerService swaggerService;
+
     @Override
     public Date getCurrentDate() {
+
         return new Date();
     }
 }
